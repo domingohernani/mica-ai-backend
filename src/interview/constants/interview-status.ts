36ed9e29ec150d52fdaa3b86bb262c73a3aaefe9
domingohernani <dev.hernani.domingo@gmail.com>
2026-05-23 23:44:34 +0800

    feat: add InterviewStatus enum for interview application states

---
+++ src/interview/constants/interview-status.ts
@@ -0,0 +1,8 @@
+export enum InterviewStatus {
+  NEW_APPLICATION = 'New Application',
+  IN_REVIEW = 'In Review',
+  SCHEDULED = 'Scheduled',
+  COMPLETED_INTERVIEW = 'Completed Interview',
+  REJECTED = 'Rejected',
+  HIRED = 'Hired',
+}
